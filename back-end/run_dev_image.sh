8c447a4b39cf42d6712670cca96d207624391240
gonzalo <gonzalo.b90@gmail.com>
2023-11-04 23:21:04 +0000

    ingestiong and query

--- back-end/run_dev_image.sh
+++ back-end/run_dev_image.sh
@@ -6,4 +6,12 @@ DOCKER_IMAGE=hackaton-api-dev:latest
 docker build -f Dockerfile-dev -t $DOCKER_IMAGE .
 
 # Build image with tag
-docker run -it -e ANTHROPIC_API_KEY="${ANTHROPIC_API_KEY}" -p 5000:8008 -v $(pwd):/code $DOCKER_IMAGE
+docker run -it \
+  -e ANTHROPIC_API_KEY="${ANTHROPIC_API_KEY}" \
+  -e OPENAI_API_KEY="${OPENAI_API_KEY}" \
+  -e OPENAI_API_BASE="${OPENAI_API_BASE}" \
+  -e PINECONE_API_KEY="${PINECONE_API_KEY}" \
+  -e PINECONE_ENV="${PINECONE_ENV}" \
+  -p 5000:8008 \
+  -v $(pwd):/code \
+  $DOCKER_IMAGE
