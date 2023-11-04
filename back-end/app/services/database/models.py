d0d4f2dc33370f74bbda857f9c3f41587e03f517
gonzalo <gonzalo.b90@gmail.com>
2023-11-04 14:52:09 +0000

    start files on backend

---
+++ back-end/app/services/database/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
